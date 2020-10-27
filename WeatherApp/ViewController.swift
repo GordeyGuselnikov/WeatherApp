7cb5b7c0ca884637362d9f87015b1849e29e61f1
Гордей Гусельников <guselnikovgordey@icloud.com>
2020-10-27 14:11:56 +0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Гордей Гусельников on 27.10.2020.
+//  Copyright © 2020 gordeyStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
